a9bc7d8d094b07b27ff8df317f0a486b0f26d193
DestroyerOfCode <marius.babkovic9@gmail.com>
2021-06-24 16:36:29 +0200

    [feature] added cors protection and role requierements

--- src/main/java/com/doge/tip/configuration/SpringWebSecurity.java
+++ src/main/java/com/doge/tip/configuration/SpringWebSecurity.java
@@ -1,6 +1,7 @@
 package com.doge.tip.configuration;
 
 import com.doge.tip.service.CustomUserDetailsService;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -10,6 +11,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import javax.annotation.Resource;
 
@@ -30,12 +35,18 @@ public class SpringWebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .cors().disable()
+            .antMatcher("/**")
             .authorizeRequests()
-            .antMatchers("/**")
+                .antMatchers("/user/**")
+                    .hasRole("admin")
+            .anyRequest()
             .authenticated()
             .and()
             .httpBasic()
+                .and()
+                .formLogin()
+            .and()
+            .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
         ;
     }
 
@@ -50,5 +61,19 @@ public class SpringWebSecurity extends WebSecurityConfigurerAdapter {
         authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
 }
